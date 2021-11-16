f19a381dc83c4c02a55de4a071e855eba5c015d3
jamesjatupat <94443136+jamesjatupat@users.noreply.github.com>
2021-11-16 20:52:20 +0700

    Getting started

---
+++ app.js
@@ -0,0 +1,25 @@
+// function b(){
+//     console.log('Called B');
+// }
+
+// b();
+
+// console.log(a);
+
+// var a = 'Hello Wolrd!';
+
+// console.log(a);
+
+function a(){
+    b();
+    console.log("test");
+    var c;
+}
+
+function b(){
+    var d = 'Hello';
+    console.log(d);
+}
+a();
+// var d;
+// console.log(d);
