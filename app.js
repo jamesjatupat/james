16669db402882a91baea6b7d6f10efe66f0f117a
jamesjatupat <popperryly@hotmail.com>
2021-11-17 20:51:30 +0700

    click event

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
-// function b(){
-//     console.log('Called B');
-// }
-
-// b();
-
-// console.log(a);
-
-// var a = 'Hello Wolrd!';
-
-// console.log(a);
-
-function a(){
-    b();
-    console.log("test");
-    var c;
+//long running function
+function waitThreeSeconds(){
+    var ms = 3000 + new Date().getTime();
+    while (new Date() < ms){}
+    console.log('finished function');
 }
 
-function b(){
-    var d = 'Hello1';
-    console.log(d);
+function clickHandler(){
+    console.log('click event!');
 }
-a();
-// var d;
-// console.log(d);
+
+//listen for the click event
+document.addEventListener('click',clickHandler);
+
+waitThreeSeconds();
+console.log('finished execution');
